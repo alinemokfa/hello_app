1064142a5c7c86d05f83907fa45382229eb93e01
Aline M <alinemokfa@hotmail.co.uk>
2019-09-28 14:20:30 +0000

    Add goodbye exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world! let's dismantle oppressive systems!"
+    render html: "olá, mundo! let's dismantle oppressive systems!"
+  end
+  
+  def goodbye
+    render html: "até logo, mundo! let's dismantle oppressive systems!"
   end  
 end
